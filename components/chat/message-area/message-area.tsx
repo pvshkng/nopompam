d4ce640d37afbd13ae100ae4eee4e5db52c5447e
Puvish <p.kungsapattarakul@gmail.com>
2025-07-26 23:45:15 +0700

    fixed animation lag

--- components/chat/message-area/message-area.tsx
+++ components/chat/message-area/message-area.tsx
@@ -86,7 +86,7 @@ export default function PureMessageArea(props: MessageAreaProps) {
           />
         </div>
       ))}
-      {status === "submitted" && <MessageSkeleton />}
+      {status == "submitted" && <MessageSkeleton />}
     </div>
   );
 }
