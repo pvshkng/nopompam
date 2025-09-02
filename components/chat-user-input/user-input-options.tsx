9e6d8ea3825c4aa345ef6e977776b0b00d8c0f94
Puvish <p.kungsapattarakul@gmail.com>
2025-09-02 23:01:24 +0700

    added llm drawer

--- components/chat-user-input/user-input-options.tsx
+++ components/chat-user-input/user-input-options.tsx
@@ -2,9 +2,12 @@ import { cn } from "@/lib/utils";
 import { ModelSelector } from "@/components/chat-model-selector";
 import { GalleryHorizontalEnd } from "@/components/icons/gallery-horizontal-end";
 import { Attachment } from "./attachment";
-
+import { Cog } from "lucide-react";
+import { useState } from "react";
+import { LLMDrawer } from "../prototype/llm-drawer";
 export const UserInputOptions = (props: any) => {
   const { dossierOpen, setDossierOpen, model, setModel } = props;
+  const [optionOpen, setOptionOpen] = useState(false);
   return (
     <>
       <div
@@ -20,12 +23,28 @@ export const UserInputOptions = (props: any) => {
           <div className="flex flex-row h-full gap-1">
             <ModelSelector model={model} setModel={setModel} />
             <Attachment />
+            <button
+              className={cn(
+                "gap-1",
+                "flex flex-row bg-stone-700 size-[16px] items-center justify-center",
+                "text-sm text-gray-500 hover:text-gray-700 h-full w-8"
+              )}
+              onClick={() => {
+                setOptionOpen(true);
+              }}
+            >
+              <Cog width={16} height={16} stroke={"white"} />
+            </button>
           </div>
           <p className="text-xs text-stone-500">
-            Made with ❤︎ by <a href="https://puvish.dev" target="_blank" className="underline">puvish.dev</a>
+            Made with ❤︎ by{" "}
+            <a href="https://puvish.dev" target="_blank" className="underline">
+              puvish.dev
+            </a>
           </p>
         </div>
       </div>
+      <LLMDrawer open={optionOpen} onOpenChange={setOptionOpen} />
     </>
   );
 };
