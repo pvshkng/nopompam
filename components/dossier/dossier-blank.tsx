7993011d5ebbc5b6d838142221c0f67d0666586a
Puvish <p.kungsapattarakul@gmail.com>
2025-10-06 22:55:58 +0700

    added carousel

--- components/dossier/dossier-blank.tsx
+++ components/dossier/dossier-blank.tsx
@@ -4,12 +4,25 @@ import { cn } from "@/lib/utils";
 import { getArtifacts } from "@/lib/mongo/artifact-store";
 import { DossierBrowser } from "@/components/dossier/dossier-browser";
 import { useDossierStore } from "@/lib/stores/dossier-store";
+import { X, House, NotebookPen, Save, Loader2 } from "lucide-react";
+import { EmblaCarousel } from "@/components/embla-carousel";
+import { EmblaOptionsType } from "embla-carousel";
 
 const template = [
   { kind: "text", prompt: ["prompt 1", "prompt 2"] },
   { kind: "text", prompt: ["prompt 1", "prompt 2"] },
   { kind: "text", prompt: ["prompt 1", "prompt 2"] },
 ];
+
+const iconMapping: Record<string, any> = {
+  text: NotebookPen,
+  sheet: NotebookPen,
+  //image: House,
+};
+
+const OPTIONS: EmblaOptionsType = { slidesToScroll: "auto" };
+const SLIDE_COUNT = 10;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 const PureBlankDocument = (props: any) => {
   const { messages = [] } = props;
@@ -23,12 +36,34 @@ const PureBlankDocument = (props: any) => {
 
   return (
     <div className={cn("flex flex-col size-full items-center justify-center")}>
-      <p className={cn("text-xs font-semibold text-stone-400")}>
-        There's no documents in the dossier yet
+      <p className={cn("text-sm text-stone-400")}>
+        Document in this chat
       </p>
-      <p className={cn("text-xs font-semibold text-stone-300")}>
-        Ask nopompam to create one
-      </p>
+      {chatDocuments.length > 0 ? (
+        <>
+          <div className={cn("flex flex-row", "w-full gap-1 m-1 p-4")}>
+            <EmblaCarousel slides={chatDocuments} options={OPTIONS} />
+            {/* {chatDocuments.map((d, i) => (
+              <div
+                key={i}
+                className="w-32 p-1 bg-neutral-50 rounded-sm shadow-md"
+              >
+                <span className="text-xs">{d.title}</span>
+              </div>
+            ))} */}
+          </div>
+        </>
+      ) : (
+        <>
+          <p className={cn("text-xs font-semibold text-stone-400")}>
+            There's no documents in the dossier yet
+          </p>
+          <p className={cn("text-xs font-semibold text-stone-300")}>
+            Ask nopompam to create one
+          </p>
+        </>
+      )}
+
       {/* <p>{chatDocuments.map((d) => <p>{d.title} {d.id}</p>)}</p> */}
     </div>
   );
---
+++ components/embla-carousel/index.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { EmblaOptionsType } from "embla-carousel";
+import { DotButton, useDotButton } from "./embla-dot-button";
+import { PrevButton, NextButton, usePrevNextButtons } from "./embla-arrow";
+import useEmblaCarousel from "embla-carousel-react";
+import { cn } from "@/lib/utils";
+import { X, House, NotebookPen, Save, Loader2 } from "lucide-react";
+import "./embla.css";
+
+type PropType = {
+  slides: any[];
+  options?: EmblaOptionsType;
+};
+
+export const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  return (
+    <section className="embla w-full">
+      <div className="embla__viewport !w-full" ref={emblaRef}>
+        <div className="embla__container gap-2 px-4 w-full !mx-auto justify-center">
+          {slides.map((doc, index) => (
+            <div
+              className={cn(
+                "embla__slide rounded-md border border-stone-300 h-32 w-4 max-w-80 p-3",
+                "flex flex-col"
+              )}
+              key={index}
+            >
+              <div className="items-center text-sm font-semibold truncate">
+                <span>{doc.title}</span>
+              </div>
+              <div className="text-xs line-clamp-5">
+                {doc.content.replace(/<[^>]+>/g, "")}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls !flex !w-full !m-0 !p-0 justify-center">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={"embla__dot".concat(
+                index === selectedIndex ? " embla__dot--selected" : ""
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
