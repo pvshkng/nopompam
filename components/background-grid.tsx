84c0419727ce7db529d887d6f014ad07c737c92d
Puvish <p.kungsapattarakul@gmail.com>
2025-11-24 22:33:31 +0700

    fixed type

---
+++ components/background-grid.tsx
@@ -0,0 +1,21 @@
+export function GridBackground() {
+  return (
+    <>
+      <div
+        className="absolute top-0 left-0 z-0 w-full h-full"
+        style={{
+          pointerEvents: "none",
+        }}
+      >
+        <div
+          className="absolute inset-0 z-0"
+          style={{
+            backgroundImage: `
+        radial-gradient(circle at 30% 70%, rgba(173, 216, 230, 0.35), transparent 60%),
+        radial-gradient(circle at 70% 30%, rgba(255, 182, 193, 0.4), transparent 60%)`,
+          }}
+        />
+      </div>
+    </>
+  );
+}
