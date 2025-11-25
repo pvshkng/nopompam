7b369cdfe087f300f0bb914279fc323f79d6d632
Puvish <p.kungsapattarakul@gmail.com>
2025-11-25 21:52:01 +0700

    refact

--- components/chat-user-input/user-input-options.tsx
+++ components/chat-user-input/user-input-options.tsx
@@ -1,40 +1,15 @@
 import { cn } from "@/lib/utils";
 import { ModelSelector } from "@/components/chat-model-selector";
-import { GalleryHorizontalEnd } from "@/components/icons/gallery-horizontal-end";
-import { Attachment } from "./attachment";
-import { Cog } from "lucide-react";
-import { useState } from "react";
-import { LLMDrawer } from "../prototype/llm-drawer";
+
 export const UserInputOptions = (props: any) => {
-  const { dossierOpen, setDossierOpen, model, setModel } = props;
-  const [optionOpen, setOptionOpen] = useState(false);
+  const { model, setModel } = props;
+
   return (
     <>
-      <div
-        className={cn(
-          "flex",
-          "m-1 h-full w-full",
-          //"border border-stone-700",
-          "max-w-[800px]"
-        )}
-      >
-        {/* Index section */}
+      <div className={cn("flex", "m-1 h-full w-full", "max-w-[800px]")}>
         <div className="flex flex-row gap-1 my-0 w-full items-center justify-between">
           <div className="flex flex-row h-full gap-1">
             <ModelSelector model={model} setModel={setModel} />
-            {/* <Attachment /> */}
-            {/* <button
-              className={cn(
-                "gap-1",
-                "flex flex-row bg-stone-700 size-[16px] items-center justify-center",
-                "text-sm text-gray-500 hover:text-gray-700 h-full w-8"
-              )}
-              onClick={() => {
-                setOptionOpen(true);
-              }}
-            >
-              <Cog width={16} height={16} stroke={"white"} />
-            </button> */}
           </div>
           <p className="text-xs text-stone-500">
             Made with ❤︎ by{" "}
@@ -44,7 +19,6 @@ export const UserInputOptions = (props: any) => {
           </p>
         </div>
       </div>
-      <LLMDrawer open={optionOpen} onOpenChange={setOptionOpen} />
     </>
   );
 };
